81ecad2d8268f054fd78401ff52e70bf1135aaa1
ynonp <ynonperek@gmail.com>
2020-04-07 21:34:57 +0300

    removed duplicated files

--- public/cljs-runtime/acme.frontend.app.js
+++
@@ -1,7 +0,0 @@
-goog.provide('acme.frontend.app');
-goog.require('cljs.core');
-acme.frontend.app.init = (function acme$frontend$app$init(){
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello World"], 0));
-});
-
-//# sourceMappingURL=acme.frontend.app.js.map
